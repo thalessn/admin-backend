0acebe29525ee324a968b084bda39cb8beea595e
Thales Nascimento <thales.silva.nascimento@gmail.com>
2023-05-04 18:01:59 -0300

    feat: add value object

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM node:18.14.2-slim
 
+RUN apt update && apt install -y --no-install-recommends \
+  git \
+  default-jre
+
+ENV JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+
 USER node
 
 WORKDIR /home/node/app
 
-CMD [ "sh", "-c", "npm install && tail -f /dev/null" ]
+CMD [ "sh", "-c", "npm install && tail -f /dev/null" ] 
---
+++ src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -0,0 +1,29 @@
+import InvalidUuidError from "../../../errors/invalid-uuid-error";
+import UniqueEntityId from "../unique-entity-id.vo";
+import { validate as uuidValidate } from "uuid";
+
+describe("Unique Entity Id", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+
+  it("should throw an err if invalid uuid is passed", () => {
+    expect(() => new UniqueEntityId("fake uuid")).toThrow(
+      new InvalidUuidError()
+    );
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a valid uuid as parameter", () => {
+    //const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const uuid = "4531d7fa-d4f8-4d2f-9d4f-462137ad2fdb";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should generate a valid uuid if the constructor is empty", () => {
+    //const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const vo = new UniqueEntityId();
+    expect(uuidValidate(vo.value)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
