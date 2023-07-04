20d4758b7ed4bec0719fe3aa1af76808d338f214
Thales Nascimento <thales.silva.nascimento@gmail.com>
2023-07-03 21:28:15 -0300

    chore: add jest config for multiple project

---
+++ src/nestjs/jest.config.ts
@@ -0,0 +1,19 @@
+export default {
+  displayName: {
+    name: 'nestjs',
+    color: 'magentaBright',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@tsn/micro\\-videos/(.*)$':
+      '<rootDir>/../../../node_modules/@tsn/micro-videos/dist/$1',
+  },
+};
