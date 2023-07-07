227168016abc9c2e28941242020ba776dba3ab99
Thales Nascimento <thales.silva.nascimento@gmail.com>
2023-07-07 17:15:48 -0300

    test: add category model test

--- src/@core/src/category/infra/db/sequelize/category-model.spec.ts
+++ src/@core/src/category/infra/db/sequelize/category-model.spec.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize-typescript";
+import { DataType, Sequelize } from "sequelize-typescript";
 import { CategoryModel } from "./category-model";
 
 describe("CategoryModel Unit Tests", () => {
@@ -31,5 +31,57 @@ describe("CategoryModel Unit Tests", () => {
     };
     const category = await CategoryModel.create(arrange);
     expect(category.toJSON()).toStrictEqual(arrange);
+  });
+
+  it("mapping props", () => {
+    const attributesMaps = CategoryModel.getAttributes();
+    const attributes = Object.keys(CategoryModel.getAttributes());
+    expect(attributes).toStrictEqual([
+      "id",
+      "name",
+      "description",
+      "is_active",
+      "created_at",
+    ]);
+
+    const idAttr = attributesMaps.id;
+    expect(idAttr).toMatchObject({
+      field: "id",
+      fieldName: "id",
+      primaryKey: true,
+      type: DataType.UUID(),
+    });
+
+    const nameAttr = attributesMaps.name;
+    expect(nameAttr).toMatchObject({
+      field: "name",
+      fieldName: "name",
+      allowNull: false,
+      type: DataType.STRING(255),
+    });
+
+    const descriptionAttr = attributesMaps.description;
+    expect(descriptionAttr).toMatchObject({
+      field: "description",
+      fieldName: "description",
+      allowNull: true,
+      type: DataType.TEXT(),
+    });
+
+    const isActiveAttr = attributesMaps.is_active;
+    expect(isActiveAttr).toMatchObject({
+      field: "is_active",
+      fieldName: "is_active",
+      allowNull: false,
+      type: DataType.BOOLEAN(),
+    });
+
+    const createdAtAttr = attributesMaps.created_at;
+    expect(createdAtAttr).toMatchObject({
+      field: "created_at",
+      fieldName: "created_at",
+      allowNull: false,
+      type: DataType.DATE(),
+    });
   });
 });
