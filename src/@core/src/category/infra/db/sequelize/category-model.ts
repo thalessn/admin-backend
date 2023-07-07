cddd424d9539ad8800b319bb5f79fee3780ddf0e
Thales Nascimento <thales.silva.nascimento@gmail.com>
2023-07-07 16:42:47 -0300

    fix: add declare varible before category model properties

--- src/@core/src/category/infra/db/sequelize/category-model.ts
+++ src/@core/src/category/infra/db/sequelize/category-model.ts
@@ -18,17 +18,17 @@ type CategoryModelProperties = {
 export class CategoryModel extends Model<CategoryModelProperties> {
   @PrimaryKey
   @Column({ type: DataType.UUID })
-  id: string;
+  declare id: string;
 
   @Column({ allowNull: false, type: DataType.STRING(255) })
-  name: string;
+  declare name: string;
 
   @Column({ type: DataType.TEXT })
-  description: string | null;
+  declare description: string | null;
 
   @Column({ allowNull: false, type: DataType.BOOLEAN })
-  is_active: boolean;
+  declare is_active: boolean;
 
   @Column({ allowNull: false, type: DataType.DATE })
-  create_at: Date;
+  declare created_at: Date;
 }
