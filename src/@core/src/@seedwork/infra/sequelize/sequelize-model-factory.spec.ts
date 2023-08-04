cb1e4620bc7d9be6c5b36f8fbb3b549f5e2dd7fe
Thales Nascimento <thales.silva.nascimento@gmail.com>
2023-08-04 10:18:56 -0300

    feat: add create method to SequelizeFactoryMethod

---
+++ src/@core/src/@seedwork/infra/sequelize/sequelize-model-factory.spec.ts
@@ -0,0 +1,73 @@
+import {
+  Table,
+  Column,
+  PrimaryKey,
+  Model,
+  DataType,
+  Sequelize,
+} from "sequelize-typescript";
+import { SequelizeModelFactory } from "./sequelize-model-factory";
+import _chance from "chance";
+import { validate as uuidValidate } from "uuid";
+
+const chance = _chance();
+
+@Table({})
+class StubModel extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUID })
+  declare id;
+
+  @Column({ allowNull: false, type: DataType.STRING(255) })
+  declare name;
+
+  static mockFactory = jest.fn(() => ({
+    id: chance.guid({ version: 4 }),
+    name: chance.word(),
+  }));
+
+  static factory() {
+    return new SequelizeModelFactory(StubModel, StubModel.mockFactory);
+  }
+}
+describe("SequelizeModelFactory Tests", () => {
+  let sequelize: Sequelize;
+
+  beforeAll(
+    () =>
+      (sequelize = new Sequelize({
+        dialect: "sqlite",
+        host: ":memory:",
+        logging: false,
+        models: [StubModel],
+      }))
+  );
+
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+  test("create method", async () => {
+    let model = await StubModel.factory().create();
+    expect(uuidValidate(model.id));
+    expect(model.name).not.toBeNull();
+    expect(StubModel.mockFactory).toHaveBeenCalled();
+
+    let modelFound = await StubModel.findByPk(model.id);
+    expect(model.id).toBe(modelFound.id);
+
+    model = await StubModel.factory().create({
+      id: "0d379d08-996b-4ffa-a44e-eb7f1a6d623c",
+      name: "test",
+    });
+    expect(model.id).toBe("0d379d08-996b-4ffa-a44e-eb7f1a6d623c");
+    expect(model.name).toBe("test");
+    expect(StubModel.mockFactory).toHaveBeenCalledTimes(1);
+
+    modelFound = await StubModel.findByPk(model.id);
+    expect(model.id).toBe(modelFound.id);
+  });
+});
--- src/@core/src/@seedwork/infra/sequelize/sequelize-model-factory.ts
+++ src/@core/src/@seedwork/infra/sequelize/sequelize-model-factory.ts
@@ -2,7 +2,7 @@ export class SequelizeModelFactory {
   constructor(private model, private factoryProps: () => any) {}
 
   async create(data?) {
-    this.model.create(data ? data : this.factoryProps());
+    return this.model.create(data ? data : this.factoryProps());
   }
 
   make() {}
